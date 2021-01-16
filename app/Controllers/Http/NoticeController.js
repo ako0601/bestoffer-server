5dec296ebd1b3b4baf0852ec3b458e95a0a2f707
kjunha <kjunha77@gmail.com>
2021-01-16 16:54:17 +0900

    add Notice Controller

--- app/Controllers/Http/NoticeController.js
+++ app/Controllers/Http/NoticeController.js
@@ -1,6 +1,61 @@
-'use strict'
+"use strict";
+
+const Firestore = use("App/Models/Firestore");
+const { validate } = use("Validator");
+const firestore = new Firestore();
+const db = firestore.db();
+const noticeReference = db.collection("notice");
 
 class NoticeController {
+  async create({ request, response }) {
+    const rules = {
+      writer: "required",
+      title: "required",
+    };
+
+    const data = request.only(["writer", "title", "used"]);
+    const validation = await validate(data, rules);
+    if (validation.fails()) {
+      return response.status(206).json({
+        status: false,
+        message: validation.messages()[0].message,
+        data: null,
+      });
+    }
+    let create = await noticeReference.add({
+      writer: data.writer,
+      title: data.title,
+      used: data.used ? 1 : 0,
+    });
+    if (create) {
+      return response.status(201).json({
+        status: true,
+        message: "User created successfully",
+        data: null,
+      });
+    }
+  }
+  async list({ request, response }) {
+    let notices = [];
+    // reference (collection) -> where -> then(snapshot => {})
+    // reference (collection) -> doc('id') -> get()
+    await noticeReference.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        let id = doc.id;
+        let notice = doc.data();
+        notices.push({
+          id,
+          ...notice,
+        });
+      });
+    });
+
+    return response.status(201).json({
+      status: true,
+      message: "User created successfully",
+      data: notices,
+    });
+  }
 }
 
-module.exports = NoticeController
+module.exports = NoticeController;
